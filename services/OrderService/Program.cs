e261f24872acbae550e4753a7a6fb50721dcd8cf
Behrang Bina <behrangbina@hotmail.com>
2026-05-30 19:41:31 +1000

    cI: fix api issues

--- services/OrderService/Program.cs
+++ services/OrderService/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using OrderService.Data;
 using OrderService.Producers;
 
--- services/UserService/Program.cs
+++ services/UserService/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using UserService.Data;
 
 namespace UserService;
